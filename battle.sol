d4f66661758aed1f057ca725b5e6bfafb1439966
Vivian <vivian.culn@windriver.com>
2024-08-21 05:56:32 -0300

    fist commit

---
+++ battle.sol
@@ -0,0 +1,45 @@
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract PokeDio is ERC721 {
+    struct Pokemon {
+        string name;
+        uint level;
+        string img;
+    }
+
+    Pokemon[] public pokemons;
+    address public gameOwner;
+
+    constructor() ERC721("PokeDio", "PKD") {
+        gameOwner = msg.sender;
+    }
+
+
+    modifier onlyOwner(uint _monsterId) {
+        require(ownerOf(_monsterId) == msg.sender, "Apenas o dono pode batalhar com este Pokemon");
+        _;
+    }
+
+    function battle(uint _attackingPokemon, uint _defendingPokemon) public onlyOwner(_attackingPokemon){
+        Pokemon storage attacker = pokemons[_attackingPokemon];
+        Pokemon storage defender = pokemons[_defendingPokemon];
+
+        if (attacker.level >= defender.level) {
+            attacker.level += 2;
+            defender.level += 1;
+        } else {
+            attacker.level += 1;
+            defender.level += 2;
+        }
+    }
+
+    function createNewPokemon(string memory _name, address _to, string memory _img) public {
+        require(msg.sender == gameOwner, "Apenas o dono do jogo pode criar novos Pokemons");
+        uint id = pokemons.length;
+        pokemons.push(Pokemon(_name, 1, _img));
+        _safeMint(_to, id);
+    }
+}
